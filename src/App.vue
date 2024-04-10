746468fdd0ec9ad5999f601be0effe561cb28a5a
Mebice <f7gl32x9v@gmail.com>
2024-04-11 00:06:26 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue';
+</script>
+
+<template>
+    <div class="header">
+        <Header />
+    </div>
+
+    <RouterView />
+
+</template>
+
+<style lang="scss" scoped>
+.header{
+height: 70px;
+background-color: #ecb5b5;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+
+<RouterLink to="/">Home</RouterLink>
+
+</template>
+
+<style lang="scss" scoped>
+</style>
