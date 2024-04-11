9d8aacc521e8a6832c62504d9d4f5753555499c5
Mebice <f7gl32x9v@gmail.com>
2024-04-11 18:45:04 +0800

    message

--- src/views/aaa.vue
+++ src/views/aaa.vue
@@ -1,13 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 const currentPage3 = ref(5)
-const currentPage4 = ref(4)
-const pageSize2 = ref(100)
 const pageSize3 = ref(100)
-const pageSize4 = ref(100)
-const small = ref(false)
-const background = ref(false)
-const disabled = ref(false)
 
 const handleSizeChange = (val) => {
   console.log(`${val} items per page`)
@@ -34,7 +28,6 @@ const handleCurrentChange = (val) => {
   height: 200px;
   background-color: #e14545;
 }
-
 .el-pagination {
   justify-content: center; //居中
   //f1oat:1eft;居左
@@ -67,4 +60,5 @@ const handleCurrentChange = (val) => {
 :deep(.el-pagination__goto),:deep(.el-input__inner) {
   color: #ff0000;
 }
+
 </style>
