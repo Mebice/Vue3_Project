04700ec82e4eb7b77961572e6d643cc042596de5
Mebice <f7gl32x9v@gmail.com>
2024-05-13 12:57:17 +0800

    token 攔截器

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,6 @@
 //axios基礎的封装
 import axios from 'axios'
+import { useUserStore } from '@/stores/user'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 
@@ -8,11 +9,18 @@ baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
 timeout:5000
 })
 
-//攔截器
+// 攔截器
 
-//axiosi請求攔截器
+// axiosi請求攔截器
 httpInstance.interceptors.request.use(config =>{
-return config
+    // 從pinia獲取token數據
+    const userStore = useUserStore()
+    // 按照後端要求拼接token數據
+    const token = userStore.userInfo.token
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
 }, e => Promise.reject(e))
 
 // axios響應式攔截器
